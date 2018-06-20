fd1ed25cf2381dc81a48ad364ce5c0b27fb59fa1
shobanavv <shobanavv@gmail.com>
2018-06-20 13:46:37 -0700

    graph.py added

---
+++ src/draw.py
@@ -0,0 +1,42 @@
+import math
+
+from bokeh.io import show, output_file
+from bokeh.plotting import figure
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.palettes import Spectral8
+
+N = 12
+node_indices = list(range(N))
+
+plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+              tools='', toolbar_location=None)
+
+debug_palette = Spectral8
+debug_palette.append('0000ff')
+debug_palette.append('ff00ff')
+debug_palette.append('f0f0ff')
+debug_palette.append('ff0f0f')
+
+graph = GraphRenderer()
+
+graph.node_renderer.data_source.add(node_indices, 'index')
+graph.node_renderer.data_source.add(Spectral8, 'color')
+graph.node_renderer.glyph = Oval(height=0.06, width=0.2, fill_color='color')
+
+graph.edge_renderer.data_source.data = dict(
+    start=[0]*N,
+    end=node_indices)
+
+### start of layout code
+# setting position of the vertexes
+circ = [i*2*math.pi/N for i in node_indices]
+x = [math.cos(i) for i in circ]
+y = [math.sin(i) for i in circ]
+
+graph_layout = dict(zip(node_indices, zip(x, y)))
+graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+plot.renderers.append(graph)
+
+output_file('graph.html')
+show(plot)
